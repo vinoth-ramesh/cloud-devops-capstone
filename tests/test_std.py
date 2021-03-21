83b9204bd62c1337dd1ecb8441964f3865971e72
Vinoth <rvinoth2005@gmail.com>
2021-03-21 21:07:43 +0530

    CICircle changes

--- tests/test_std.py
+++ tests/test_std.py
@@ -13,18 +13,18 @@ def test_basic_url(supply_url):
     response = requests.get(url)
     response_body = response.json()
     assert response.status_code == 200
-    assert response_body == "Please use /dbz URI to view content"
+    assert response_body == "Please use /std URI to view content"
 
 def test_post_request(supply_url):
-    url = supply_url + "/dbz"
+    url = supply_url + "/std"
     headers = {'Content-Type': 'application/json'}
-    payload = { 'name': 'Goku', 'powerlevel': '150,000,000'}
+    payload = { 'name': 'User1', 'ranklevel': '25'}
     response = requests.post(url, headers=headers, data=json.dumps(payload, indent=4))
     assert response.status_code == 200
     print(response.text)
 
 def test_invalid_entry(supply_url):
-    url = supply_url + "/piccolo"
+    url = supply_url + "/User2"
     response = requests.get(url)
     assert response.status_code == 404
     print(response.text)
